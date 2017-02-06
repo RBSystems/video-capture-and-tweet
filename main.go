92796b532daffbc48bbe4564306389e56c289e5c
joseph <joseph.blodgett@gmail.com>
2017-02-06 15:55:40 -0700

    Testing the exec command

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"os/exec"
+	"strings"
+)
 
+var configuration Configuration
+
+func main() {
+	config := flag.String("c", "./config.json", "configuration file")
+	//interval := flag.Int("i", 500, "Increment (in seconds)")
+
+	configuration = getConfiguration(*config)
+	GetAndConvertFrame()
+
+}
+
+func GetAndConvertFrame() error {
+	vals := strings.Split(configuration.CaptureFrameCommand, " ")
+	err := exec.Command(vals[0], vals[1:]...).Run()
+	if err != nil {
+		return err
+	}
+	return nil
 }
