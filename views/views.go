d019f47959995c86f2eba3a07036994453e38f95
Jesse Millar <hellojessemillar@gmail.com>
2017-02-15 15:46:41 -0700

    Sexy toggle

--- views/views.go
+++ views/views.go
@@ -8,7 +8,16 @@ import (
 )
 
 func Main(context echo.Context) error {
-	log.Println("Returning page")
-
+	log.Println("Returning main page")
 	return context.Render(http.StatusOK, "main", "")
+}
+
+func Toggle(context echo.Context) error {
+	log.Println("Returning toggle page")
+	return context.Render(http.StatusOK, "toggle", "")
+}
+
+func Approve(context echo.Context) error {
+	log.Println("Returning approval page")
+	return context.Render(http.StatusOK, "approve", "")
 }
