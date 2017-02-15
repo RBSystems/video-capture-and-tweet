1a3c8e9041c82277fee2378350229bc33356a4f6
Jesse Millar <hellojessemillar@gmail.com>
2017-02-15 16:09:51 -0700

    Might be working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,38 @@
-function init() {
-	console.log("Loaded");
+var isTweeting = false;
+
+document.addEventListener("DOMContentLoaded", function (event) {
+	document.querySelector("input[name=toggle]").addEventListener("change", function() {
+		console.log("Toggled");
+
+		if (isTweeting) {
+			stopTweeting();
+		} else {
+			startTweeting();
+		}
+	});
+})
+
+function startTweeting() {
+	httpGetAsync("http://av-capture-linux.byu.edu/tweeter/start");
+}
+
+function stopTweeting() {
+	httpGetAsync("http://av-capture-linux.byu.edu/tweeter/stop");
+}
+
+function getTweetStatus() {
+	httpGetAsync("http://av-capture-linux.byu.edu/tweeter/status", function(status) {
+		isTweeting = status;
+	});
+}
+
+function httpGetAsync(theUrl, callback) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+			callback(xmlHttp.responseText);
+	}
+
+	xmlHttp.open("GET", theUrl, true); // true for asynchronous
+	xmlHttp.send(null);
 }
