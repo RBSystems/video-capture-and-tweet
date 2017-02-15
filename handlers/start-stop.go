ce367c1a7517de9bdff5c87b636a83bafebd74c1
joseph <joseph.blodgett@gmail.com>
2017-02-15 12:32:21 -0700

    Adding interval suport

--- handlers/start-stop.go
+++ handlers/start-stop.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/byuoitav/video-capture-and-tweet/tweeter"
@@ -10,6 +11,12 @@ import (
 
 //Start starts the tweeting
 func Start(context echo.Context) error {
+	var err error
+	tweeter.Interval, err = strconv.Atoi(context.QueryParam("interval"))
+	if err != nil {
+		return context.JSON(http.StatusBadRequest, "invalid interval")
+	}
+
 	tweeter.StartChannel <- true
 	return context.JSON(http.StatusOK, "Tweeting started")
 }
